4ffc972493ee1ed0b512d113d5a037bb19ff431e
gmp26 <gmp26@cam.ac.uk>
2013-03-20 20:37:54 +0000

    removed incoming

--- incoming/accordion/template/accordion/accordion.html.js
+++
@@ -1,4 +0,0 @@
-angular.module("template/accordion/accordion.html", []).run(function($templateCache){
-  $templateCache.put("template/accordion/accordion.html",
-    "<div class=\"accordion\" ng-transclude></div>");
-});
--- incoming/swiper/app.js
+++
@@ -1,5 +0,0 @@
-angular.module('app', ['swiper']).controller('AppController', function($scope) {
-  $scope.logSlide = function(index, slide) {
-    console.log('Slide end: ', index);
-  };
-});
