gmp26/Apps1

commit dc77f862777e609d9821e082db32db4e9dbe8668

added dependency for helpers/geometry.js

src/scripts/pubs/mathmo/main.ls
@@ -40,6 +40,10 @@ require {
         * 'libs/seedrandom'
           'libs/qa/helpers'
 
+    # 'libs/qa/geometry':
+    #   deps:
+    #     * 'libs/qa/helpers'
+
     'libs/qa/problems':
       deps:
         * 'libs/seedrandom'

src/scripts/main.ls
@@ -40,6 +40,9 @@ require {
         * 'libs/seedrandom'
           'libs/qa/helpers'
 
+    'libs/qa/geometry':
+      deps:
+        * 'libs/qa/helpers'
 
     'libs/qa/problems':
       deps:

commit 992c319a10a6248c97d31ba6940e0d9db42bbb14

fixing 'grunt unmask' broken by commit dc77f86

src/scripts/pubs/mathmo/main.ls
@@ -40,9 +40,9 @@ require {
         * 'libs/seedrandom'
           'libs/qa/helpers'
 
-    # 'libs/qa/geometry':
-    #   deps:
-    #     * 'libs/qa/helpers'
+    'libs/qa/geometry':
+      deps:
+        * 'libs/qa/helpers'
 
     'libs/qa/problems':
       deps: