gmp26/Apps1

commit b48fc41bba2041dfda5778b1eedc930c1fb2629e

first part of mathmo question for intersection of circles and lines

src/scripts/pubs/mathmo/services/config.ls
@@ -34,6 +34,7 @@ angular.module('app').factory 'config', ->
           "C31"
           "C32"
           "C33"
+          "C44"
 
     * title: "Sequences & series"
       topicIds:
@@ -195,6 +196,9 @@ angular.module('app').factory 'config', ->
       "C33":
         * "Parallel and perpendicular lines"
           makeLineParPerp
+      "C44":
+        * "Intersections of circles and lines"
+          makeCircLineInter
 
       "F1":
         * "Complex Arithmetic"

src/scripts/libs/qa/problems.js
@@ -609,6 +609,121 @@ function makeCircleEq()
   return qa;
 }
 
+function makeCircLineInter()
+{
+  function makeLLInter()
+  {
+    m1=rand(6);
+    m2=rand(6);
+    c1=rand(6);
+    c2=rand(6);
+
+    if (rand()) { // Artificially increase the number of parallel lines
+      m1=m2;
+    }
+
+    while(m1==m2&&c1==c2) {
+      m2=rand(6);
+      c2=rand(6);
+    }
+
+    var qString="Find whether the lines \\(";
+
+    if (rand()) {
+      qString+=lineEq1(0,c1,1,m1+c1);
+    } else {
+      qString+=lineEq2(m1,c1);
+    }
+
+    qString+="\\) and \\(";
+
+    if (rand()) {
+      qString+=lineEq1(0,c2,1,m2+c2);
+    } else {
+      qString+=lineEq2(m2,c2);
+    }
+
+    qString+="\\) intersect, and if they do, find their point of intersection."
+
+    if (m1==m2) {
+      var aString="The lines do not intersect.";
+    } else {
+      var xint=new frac(c2-c1,m1-m2);
+      var yint=new frac(m1*(c2-c1)+c1*(m1-m2),m1-m2);
+      var aString="The lines intersect in a single point, which occurs at \\(\\left("+xint.write()+","+yint.write()+"\\right)\\).";
+    }
+
+    var qa=[qString,aString];
+    return qa;
+  }
+
+  function makeCLInter() {
+    var a=rand(6);
+    var b=rand(6);
+    var r=rand(2,7);
+
+    var m=rand(6);
+    var c=rand(6);
+
+    var qString="Consider the line \\(";
+
+    if (rand()) {
+      qString+=lineEq1(0,c,1,m+c);
+    } else {
+      qString+=lineEq2(m,c);
+    }
+
+    qString+="\\) and the circle \\( "
+
+    if (rand()) {
+      qString+=circleEq1(a,b,r);
+    } else {
+      qString+=circleEq2(a,b,r);
+    }
+
+    qString+="\\). Find out how many points of intersection they have, and the location of any intersections."
+
+    // Variables to solve Ax^2 + Bx + C = 0
+    var A=m*m+1;
+    var B=-2*a+2*m*(c-b);
+    var C=(c-b)*(c-b)-r*r;
+
+    var disc=B*B-4*A*C;
+    var sq=new sqroot(disc);
+
+    if (disc>0) {
+      var aString="The line and the circle intersection in two points, specifically ";
+
+      var xint1=new frac(-B,2*A);
+      var xint2=new frac(Math.abs(sq.a),2*A);
+
+      var yint1=new frac(-B*m+2*A*c,2*A);
+      var yint2=new frac(m*Math.abs(sq.a),2*A);
+
+      // First solution
+      aString+="$$\\textstyle\\left("+xint1.write()+"+"+xint2.write()+"\\sqrt{"+sq.n+"},"+yint1.write()+"+"+yint2.write()+"\\sqrt{"+sq.n+"}\\right)$$";
+
+      // // First solution
+      // aString+=xint1.write()+"+"+new frac(sq.a,2*A).write()+"\\sqrt{"+sq.n+"}";
+      // aString+=sq.a+" "+sq.n+" and break "+sq.write();
+    } else if (disc<0) {
+      var aString="The line and the circle do not intersect in any points.";
+    } else if (disc==0) { // This never happens in practice; do we want to artificially increase it?
+      var xint=new frac(-B,2*A);
+      var yint=new frac(-B*m+c*2*A,2*A);
+      var aString="The line and the circle intersect in exactly one point, which occurs at \\(\\left("+xint.write()+","+yint.write()+"\\right)\\).";
+    } else {
+      console.log("The discriminant in the equation for x in makeCLInter is not a number.")
+    }
+
+    var qa=[qString,aString];
+    return qa;
+  }
+
+  var qa=makeCLInter();
+  return qa;
+}
+
 function makeIneq()
 {
 	function makeIneq2()

src/scripts/libs/qa/helpers.js
@@ -375,7 +375,7 @@ String.prototype.repeat = function(num)
 // Returns a number signed with + or - as a string
 // Useful for equations
 
-function signedNumber(x) // What about x = +/-1?
+function signedNumber(x)
 {
     if (x > 0) {
         return "+" + x;

commit 27643e7960c9e7c2cc3c9abf97a2b0120dbf30f0

finished mathmo question on circle/circle intersections
This commit covers the case of two intersections, along with pretty printing of answers. It also adds the appropriate unit tests, and corrects those broken in the last commit (by the updated geometry helpers).

Note that mathmo may fail if you’ve used the circle/line intersection question (introduced in commit b48fc41). This had the topicID C44, which has been renamed to C34. Solution is to clear the local storage.

src/scripts/pubs/mathmo/services/config.ls
@@ -34,7 +34,7 @@ angular.module('app').factory 'config', ->
           "C31"
           "C32"
           "C33"
-          "C44"
+          "C34"
 
     * title: "Sequences & series"
       topicIds:
@@ -196,7 +196,7 @@ angular.module('app').factory 'config', ->
       "C33":
         * "Parallel and perpendicular lines"
           makeLineParPerp
-      "C44":
+      "C34":
         * "Intersections of circles and lines"
           makeCircLineInter
 

src/scripts/libs/qa/problems.js
@@ -627,7 +627,7 @@ function makeCircLineInter()
       c2=rand(6);
     }
 
-    var qString="Find whether the lines \\(";
+    var qString="Consider the lines \\(";
 
     if (rand()) {
       qString+=lineEq1(0,c1,1,m1+c1);
@@ -643,7 +643,7 @@ function makeCircLineInter()
       qString+=lineEq2(m2,c2);
     }
 
-    qString+="\\) intersect, and if they do, find their point of intersection."
+    qString+="\\). <br><br> Find out how many points of intersection they have, and the location of any intersections."
 
     if (m1==m2) {
       var aString="The lines do not intersect.";
@@ -681,7 +681,7 @@ function makeCircLineInter()
       qString+=circleEq2(a,b,r);
     }
 
-    qString+="\\). Find out how many points of intersection they have, and the location of any intersections."
+    qString+="\\). <br><br> Find out how many points of intersection they have, and the location of any intersections."
 
     // By substitution, we can get an equation of the form Ax^ + Bx + C = 0
     // The roots are the points of intersection
@@ -743,7 +743,7 @@ function makeCircLineInter()
       r2=rand(2,7);
     }
 
-    var qString="Consider the circles with equations \\(";
+    var qString="Consider the circles \\(";
 
     if (rand()) {
       qString+=circleEq1(a1,b1,r1);
@@ -761,24 +761,57 @@ function makeCircLineInter()
 
     qString+="\\). <br><br> Find out how many points of intersection they have, and the location of any intersections."
 
-    var d=Math.sqrt((b2-b1)*(b2-b1)+(a2-a1)*(a2-a1));
+    var D=Math.sqrt((b2-b1)*(b2-b1)+(a2-a1)*(a2-a1));
+    var DD=(b2-b1)*(b2-b1)+(a2-a1)*(a2-a1);
     var R=r1+r2;
+    var RR=r1*r1-r2*r2;
+    var S=Math.abs(r1-r2);
 
-    if (d>R) {
-      var aString="The two circles do not intersect in any points.";
-    } else if (d>R) {
-      var aString="Two intersections";
+    // The formulae for the case of two intersections is cribbed from
+    // http://www.ambrsoft.com/TrigoCalc/Circles2/Circle2.htm
+
+    if (R>D&&D>S) {
+      var aString="The circles intersect in two points, which are";
+
+      var d=new sqroot(-DD*DD+2*DD*r1*r1-r1*r1*r1*r1+2*DD*r2*r2+2*r1*r1*r2*r2-r2*r2*r2*r2);
+
+      // First solution
+      aString+="$$\\left(";
+      aString+=simplifySurd((a1+a2)*DD+(a2-a1)*RR,(b1-b2)*d.a,d.n,2*DD);
+      aString+=","+simplifySurd((b1+b2)*DD+(b2-b1)*RR,(a2-a1)*d.a,d.n,2*DD);
+      aString+="\\right)";
+
+      aString+="\\qquad\\text{and}\\qquad ";
+
+      // Second solution
+      aString+="\\left(";
+      aString+=simplifySurd((a1+a2)*DD+(a2-a1)*RR,(b2-b1)*d.a,d.n,2*DD);
+      aString+=","+simplifySurd((b1+b2)*DD+(b2-b1)*RR,(a1-a2)*d.a,d.n,2*DD);
+      aString+="\\right)";
+
+      aString+="$$";
     } else if (d==R) {
       var x1=new frac(a1*R+r1*(a2-a1),R);
       var y1=new frac(b1*R+r1*(b2-b1),R);
       var aString="The circles intersect in a single point, which is \\(("+x1.write()+","+y1.write()+")\\).";
+    } else if (D>R||D<=S) {
+      var aString="The two circles do not intersect in any points.";
+    } else {
+      var aString="Uh oh";
     }
 
     var qa=[qString,aString];
     return qa;
   }
 
-  var qa=makeCCInter();
+  if (rand()) {
+    var qa=makeCLInter();
+  } else if (rand()) {
+    var qa=makeLLInter();
+  } else {
+    var qa=makeCCInter();
+  }
+
   return qa;
 }
 

test/pubs/mathmo/mathmoProblemsSpec.coffee
@@ -262,20 +262,26 @@ describe "mathmoController", ->
     [
       "makeLines2D"
       "C31"
-      "Find the equation of the line passing through \\((5,-2)\\) and \\((5,-5)\\)."
-      "$$x=5.$$"
+      "Find the equation of the line passing through \\((6,-3)\\) and \\((6,-6)\\)."
+      "$$x=6.$$"
     ]
     [
       "makeCircleEq"
       "C32"
-      "Find the centre and radius of the circle with equation$$x^2+6x+y^2+6y=6.$$"
-      "The circle has centre \\((-3,-3)\\) and radius \\(4  \\)."
+      "Find the centre and radius of the circle with equation$$x^2+8x+y^2+6y=0.$$"
+      "The circle has centre \\((-4,-3)\\) and radius \\(5  \\)."
     ]
     [
       "makeLineParPerp"
       "C33"
-      "Find the equation of the line passing through \\((0,2)\\) and perpendicular to the line \\(y=1\\)."
-      "$$x=0.$$"
+      "Find the equation of the line passing through \\((1,2)\\) and perpendicular to the line \\(y=0\\)."
+      "$$x=1.$$"
+    ]
+    [
+      "makeCircLineInter"
+      "C34"
+      "Consider the line \\(x-y+1=0\\) and the circle \\( (x+5)^2+y^2=9\\). <br><br> Find out how many points of intersection they have, and the location of any intersections."
+      "The line and the circle intersect in two points, specifically $$\\left(\\frac{-6+\\sqrt{2}}{2},\\frac{-4+\\sqrt{2}}{2}\\right)\\qquad\\text{and}\\qquad \\left(\\frac{-6-\\sqrt{2}}{2},\\frac{-4-\\sqrt{2}}{2}\\right)$$"
     ]
     [
       "makeCArithmetic"